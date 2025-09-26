73a5f98d0961005ed59384f2957d73eeeb55a60c
kalidass26 <mathesh45362@gmail.com>
2025-09-26 14:08:21 +0530

    Fix CORS to allow localhost:5173 and 5174, update email transporter to port 465 secure for Gmail

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
